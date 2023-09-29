ada96b4dcaf52f5aa9c7c4fd97ec8c10bdf08175
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2023-09-29 11:10:10 +0100

    Updated the initial WIP folder creation script

--- steps/create_folder_structure.py
+++ steps/create_folder_structure.py
@@ -24,11 +24,16 @@ def create_folder_structure(**kwargs) -> Dict:
     config = kwargs['config']
     output_path = kwargs['output_path']
 
-    folders = ['data', 'structures/holo_pmhc_tcr', 'structures/apo_pmhc', 'structures/apo_tcr']
+    # Create the folder structure as a set of folders in an array.
+    folders = ['data', 'structures/holo_pmhc_tcr/raw', 'structures/apo_pmhc/raw', 'structures/apo_tcr/raw', 'structures/holo_pmhc_tcr/clean', 'structures/apo_pmhc/clean', 'structures/apo_tcr/clean']
 
+
+    # Initialise the folder creation count.
     folder_creation_count = 0
+
+    # Iterate through the folders and create them
     for folder in folders:    
-        create_folder(f"{output_path}/folder", verbose)
+        create_folder(f"{output_path}/{folder}", verbose)
         folder_creation_count += 1
     
     action_output = {
